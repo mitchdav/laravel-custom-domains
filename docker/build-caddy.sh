5a32b9a872ef6e9e5df27d8f5b856da05f4d3582
Mitchell Davis <mdavis@atlas.dev>
2023-08-06 10:09:15 +1000

    WIP.

--- docker/build-caddy.sh
+++ docker/build-caddy.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+cd /build
+
 # Update system
 yum update -y
+yum install -y wget tar xz gzip
 
 # Download and install Go 1.20.5
 rm -rf /usr/local/go
-wget https://go.dev/dl/go1.20.5.linux-amd64.tar.gz -O go.tar.gz
+wget -q https://go.dev/dl/go1.20.5.linux-amd64.tar.gz -O go.tar.gz
 tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
 
-# Set the location for go in the environment variables
-export GOBIN=/usr/local/go/bin
+# Add go to the path
+export PATH=$PATH:/usr/local/go/bin
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /etc/profile
+source /etc/profile
 
-go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+go version
 
-go/bin/xcaddy build --with $1
+go install -v -n -a github.com/caddyserver/xcaddy/cmd/xcaddy@latest
+
+ls -al .
+ls -al /usr/local/go/bin
+
+which xcaddy
+#
+#~/go/bin/xcaddy build --with $1
